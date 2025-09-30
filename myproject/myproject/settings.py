92c3e6296ec667e2df1ae8958f469f46c747b037
umida <umidamurodbekova@gmail.com>
2025-09-30 17:21:45 +0200

    Update ALLOWED_HOSTS for Render

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -27,7 +27,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-fallback")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    'DJANGO_ALLOWED_HOSTS',
+    '127.0.0.1,localhost,study-buddy-finder-owzk.onrender.com'
+).split(',')
+
+
+
 
 
 # Application definition
